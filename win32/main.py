28784c453e416c4e26266fdec563e3977f4d0189
snow.jhung <snow.jhung@rv88.com>
2020-04-27 15:54:18 +0800

    v1.0.5 add new encode aes

--- win32/main.py
+++ win32/main.py
@@ -10,6 +10,9 @@ from sys import exit
 from ctypes import windll
 from tkinter import Tk, messagebox
 from tkinter.filedialog import askopenfilename
+from Crypto.Cipher import AES
+from binascii import hexlify, unhexlify
+import hashlib
 
 SKYPE_REG = re.compile('^Skype*')
 SKYPE_CLASS_REG = re.compile('^Chrome_WidgetWin_1*')
@@ -18,7 +21,32 @@ SKYPE_CLASS_INTERMEDIATE_REG = re.compile('Intermediate D3D Window')
 SKYPE_FOR_DESKTOP_CLASS_REG = re.compile('^CabinetWClass*')
 
 
+BS = AES.block_size
 
+def padding_pkcs5(value):
+    return str.encode(value + (BS - len(value) % BS) * chr(BS - len(value) % BS))
+
+def aes_ecb_encrypt(key, value):
+    # cryptor = AES.new(bytes.fromhex(key), AES.MODE_ECB)
+    cryptor = AES.new(key, AES.MODE_ECB)
+    padding_value = padding_pkcs5(value)    # padding content with pkcs5
+    ciphertext = cryptor.encrypt(padding_value)
+    return ''.join(['%02x' % i for i in ciphertext])
+
+def aes_ecb_decrypt(key, value):
+    # key = bytes.fromhex(key)
+    cryptor = AES.new(key, AES.MODE_ECB)
+    ciphertext = cryptor.decrypt(bytes.fromhex(value))
+    return ciphertext.decode('utf-8')
+
+
+hexstr_content = '62ea45d3499cf41e424373b145ce44dc0033609d0e989338807656ef6abcc96f'
+aes_key = b't1tSr6B1RGHvujeW'
+split_character = ' | '
+
+# expect_result = 'c1ee1f3f2d74e02706be9af78aa79ba4'.upper()
+# aes128string = aes_ecb_encrypt(aes_key, 'ABcdefGHIJ123456789KlmnopQ')
+# decrypted = aes_ecb_decrypt(aes_key, '62ea45d3499cf41e424373b145ce44dc0033609d0e989338807656ef6abcc96f')
 
 
 def handle_skype(handle_id):
@@ -27,12 +55,22 @@ def handle_skype(handle_id):
     pwd = ''
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "a:p:", [])
+        opts, args = getopt.getopt(sys.argv[1:], "a:p:c:", [])
         for o, a in opts:
             if o in ("-a", "--acc"):
                 acc = a
             elif o in ("-p", "--pwd"):
                 pwd = a
+            elif o in ("-c", '--code'):
+                _code = a
+                _ap = aes_ecb_decrypt(aes_key, _code)
+                _ap_list = _ap.split(split_character, 1)
+                if len(_ap_list) != 2:
+                    # print(_ap)
+                    raise Exception("Wrong of Split Account and Password")
+                acc = _ap_list[0]
+                pwd = _ap_list[1]
+    
         
     except getopt.GetoptError as err:
         print(err)
@@ -162,9 +200,6 @@ def handle_skype(handle_id):
             
             break
 
-        
-    
-
 
 
 def get_all_exsit_skypes(is_debug=False):
@@ -225,7 +260,8 @@ def loop_handle_fn(Skype_exe):
     _exsit_skype_ids = get_all_exsit_skypes()
     if len(_exsit_skype_ids) == 0:
         # no skype opened
-        program = Skype_exe
+        # program = Skype_exe
+        program = Skype_exe + " --secondary"
     else:
         # have skype launched
         program = Skype_exe + " --secondary"
